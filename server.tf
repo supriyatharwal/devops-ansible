92ad25375099c0042d764f4588f3e55b0b6fe050
supriyatharwal <52212718+supriyatharwal@users.noreply.github.com>
2022-09-16 16:40:30 +0530

    Update server.tf

--- server.tf
+++ server.tf
@@ -1,7 +1,8 @@
 provider "aws" {
 region = "us-east-2"
 }
-resource "aws_instance" "myawsserver" {
+/*
+  resource "aws_instance" "myawsserver" {
   ami = "ami-077e31c4939f6a2f3"
   instance_type = "t2.micro"
   key_name = "ansiblesup-key"
@@ -15,4 +16,4 @@ resource "aws_instance" "myawsserver" {
     command = "echo The servers IP address is ${self.public_ip} && echo ${self.public_ip} > /tmp/inv"
   }
 }
-
+*/
